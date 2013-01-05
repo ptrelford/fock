0e782c6ff29e1e317f66b9dceff1521b719cae10
ptrelford <ptrelford@localhost>
2013-01-05 11:30:39 +0000

    Added support for properties and events in LINQ API

--- Fock2.fs
+++ Fock2.fs
@@ -13,17 +13,43 @@ type Stub<'TAbstract when 'TAbstract : not struct> internal (calls) =
     let toArgs (args:Expression seq) =
         [| for arg in args -> Any |]
     /// Converts expression to a tuple of MethodInfo and Arg array
-    let toCall (expr:Expression) =
+    let toMethodInfo (expr:Expression) =
         match expr with
         | :? MethodCallExpression as call ->
             call.Method, toArgs call.Arguments
         | _ -> raise <| NotSupportedException()
+    let toPropertyInfo (expr:Expression) =
+        match expr with
+        | :? MemberExpression as call ->
+            let pi = call.Member :?> PropertyInfo
+            pi, [||]
+        | :? MethodCallExpression as call ->
+            let pi = 
+                call.Method.DeclaringType.GetProperties() 
+                |> Seq.find (fun pi -> pi.GetGetMethod() = call.Method)
+            pi, toArgs call.Arguments
+        | _ -> raise <| NotSupportedException()
     /// Specifies a member function of the abstract type
     member this.SetupFunc(expr:Expression<Func<'TAbstract,'TReturnValue>>) =
-        FuncBuilder<'TAbstract,'TReturnValue>(toCall expr.Body,calls)
+        FuncBuilder<'TAbstract,'TReturnValue>(toMethodInfo expr.Body,calls)
     /// Specifies a member action of the abstract type
     member this.SetupAction(expr:Expression<Action<'TAbstract>>) =
-        ActionBuilder<'TAbstract>(toCall expr.Body,calls)
+        ActionBuilder<'TAbstract>(toMethodInfo expr.Body,calls)
+    /// Specifies a property getter of the abstract type
+    member this.SetupPropertyGet(expr:Expression<Func<'TAbstract,'TReturnValue>>) =
+        let pi, args = toPropertyInfo expr.Body
+        let call = pi.GetGetMethod(), args 
+        FuncBuilder<'TAbstract, 'TReturnValue>(call,calls)
+    /// Specifies a property setter of the abstract type
+    member this.SetupPropertySet(expr:Expression<Func<'TAbstract,'TReturnValue>>) =
+        let pi, args = toPropertyInfo expr.Body
+        let call = pi.GetSetMethod(), args
+        ActionBuilder<'TAbstract>(call, calls)
+    /// Specifies an event of the abstract type as a quotation
+    member this.SetupEvent(name:string) =
+        let e = abstractType.GetEvent(name)
+        let handlers = e.GetAddMethod(), e.GetRemoveMethod()
+        EventBuilder<'TAbstract>(handlers,calls)
     /// Constructs mock builder
     new () = Stub([])
     /// Creates a generic instance of the abstract type
@@ -38,7 +64,7 @@ and ActionBuilder<'TAbstract when 'TAbstract : not struct>
         Stub<'TAbstract>((mi, (args, Raise(typeof<'TException>)))::calls)
 and FuncBuilder<'TAbstract,'TReturnValue when 'TAbstract : not struct>
     internal (call, calls) =
-    inherit ActionBuilder<'TAbstract>(call, calls)    
+    inherit ActionBuilder<'TAbstract>(call, calls)
     let mi, args = call    
     /// Specifies the return value of a method or property
     member this.Returns(value:'TReturnValue) =
@@ -46,3 +72,19 @@ and FuncBuilder<'TAbstract,'TReturnValue when 'TAbstract : not struct>
             if typeof<'TReturnValue> = typeof<unit> then Unit 
             else ReturnValue(value,typeof<'TReturnValue>)
         Stub<'TAbstract>((mi, (args, result))::calls)
+/// Generic builder for specifying event values
+and EventBuilder<'TAbstract when 'TAbstract : not struct> 
+    internal (handlers, calls) =
+    let add, remove = handlers
+    /// Specifies the published event value
+    member this.Publishes(value:IDelegateEvent<'TDelegate>) =
+        Stub<'TAbstract>((add, ([|Any|], Handler("AddHandler",value)))::
+                         (remove, ([|Any|], Handler("RemoveHandler",value)))::
+                         calls)
+
+[<Sealed>]
+type It private () =
+    /// Marks argument as matching any value
+    [<Fock.Wildcard>] static member IsAny<'TArg>() = Unchecked.defaultof<'TArg>
+    /// Marks argument as matching specific values
+    [<Fock.Predicate>] static member Is<'TArg>(f:'TArg -> bool) = Unchecked.defaultof<'TArg>
